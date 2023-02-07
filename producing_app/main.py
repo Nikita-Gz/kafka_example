3d8a316b0c5b0b56bf250e8e8d00913ce997d958
Nikita Gezha <ngez.work@gmail.com>
2023-02-07 16:00:25 +0200

    producer now publishes random names

--- producing_app/main.py
+++ producing_app/main.py
@@ -1,5 +1,19 @@
+# this program generates random names with some random delay,
+# and publishes them to kafka
+
 import time
+import random
+from faker import Faker
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers='broker:29092')
+
+fake = Faker()
 
 while True:
-  time.sleep(1)
-  print("I am a producer app!")
+  name = fake.first_name()
+  print(f'Publishing name: {name}')
+  future = producer.send('my_topic', name.encode('utf-8'))
+  result = future.get()
+  print(f'Published name: {name}')
+  time.sleep(random.randint(1, 5))
